4710d520a58c55f007ba2841a21a8c5fa71ced26
Dev122434 <68534leo@universidaddeleon.edu.mx>
2025-06-06 08:16:24 -0600

    Modificaciones

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,106 @@
+def numero_pulsado(num):
+    global operacion
+    global reset_pantalla
+
+    if reset_pantalla != False:
+        numero_pantalla.set(num)
+        reset_pantalla = False
+    else:
+        numero_pantalla.set(numero_pantalla.get() + num)
+
+num1 = 0
+contador_resta = 0
+
+def resta(num):
+    global operacion
+    global resultado
+    global num1
+    global contador_resta
+    global reset_pantalla
+
+    if (contador_resta == 0):
+        num1 = float(num)
+        resultado = num1
+    else:
+        resultado = float(resultado) - float(num)
+        numero_pantalla.set(resultado)
+        resultado = numero_pantalla.get()
+
+    contador_resta += 1
+    operacion = "resta"
+    reset_pantalla = True
+
+contador_suma = 0
+
+def suma(num):
+    global operacion
+    global resultado
+    global num1
+    global contador_suma
+    global reset_pantalla
+
+    if (contador_suma == 0):
+        num1 = float(num)
+        resultado = num1
+    else:
+        resultado = float(resultado) + float(num)
+        numero_pantalla.set(resultado)
+        resultado = numero_pantalla.get()
+
+    contador_suma += 1
+    operacion = "suma"
+    reset_pantalla = True
+
+contador_mult = 0
+
+def multiplicacion(num):
+    global operacion
+    global resultado
+    global num1
+    global contador_mult
+    global reset_pantalla
+
+    if (contador_mult == 0):
+        num1 = float(num)
+        resultado = num1
+    else:
+        resultado = float(resultado) * float(num)
+        numero_pantalla.set(resultado)
+        resultado = numero_pantalla.get()
+
+    contador_mult += 1
+    operacion = "multiplicacion"
+    reset_pantalla = True
+
+contador_div = 0
+
+def division(num):
+    global operacion
+    global resultado
+    global num1
+    global contador_div
+    global reset_pantalla
+
+    if (contador_div == 0):
+        num1 = float(num)
+        resultado = num1
+    else:
+        if (float(num) != 0):
+            resultado = float(resultado) / float(num)
+            numero_pantalla.set(resultado)
+            resultado = numero_pantalla.get()
+        else:
+            numero_pantalla.set("No se puede dividir")
+            resultado = numero_pantalla.get()
+
+    contador_div += 1
+    operacion = "division"
+    reset_pantalla = True
+
 from tkinter import *
-from funciones import *
 
 raiz = Tk()
-raiz.geometry("400x100")
+raiz.geometry("800x400")
 mi_frame = Frame(raiz)
 raiz.title("Calculadora Basica")
 mi_frame.pack()
@@ -49,5 +147,8 @@ boton_mult.grid(row=6, column=4)
 
 boton_div = Button(mi_frame, text="/", width=3, command=lambda:division(numero_pantalla.get()))
 boton_div.grid(row=6, column=5)
+
+boton_igual = Button(mi_frame, text="=", width=3, command=lambda:division(numero_pantalla.get()))
+boton_igual.grid(row=7, column=4)
 
 raiz.mainloop()
